241ed8906a1daa61c20e8e35a8106bf432f0ff12
unknown <noaydeh@hotmail.com>
2024-02-08 15:04:34 -0600

    Nuevo loader

--- src/Components/DataPerIndex/DataPerIndex.jsx
+++ src/Components/DataPerIndex/DataPerIndex.jsx
@@ -77,7 +77,24 @@ const DataPerIndex = () => {
                 <br />
                 <br />
                 <br />
-                <div className="loader"></div>
+                <div class="loader">
+                    <div class="cell d-0"></div>
+                    <div class="cell d-1"></div>
+                    <div class="cell d-2"></div>
+
+                    <div class="cell d-1"></div>
+                    <div class="cell d-2"></div>
+
+
+                    <div class="cell d-2"></div>
+                    <div class="cell d-3"></div>
+
+
+                    <div class="cell d-3"></div>
+                    <div class="cell d-4"></div>
+
+
+                </div>
             </div>
         </div>
     }
